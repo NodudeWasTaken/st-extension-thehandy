00a67b1b42c203006d5c8e02676c30e69d566e11
Nodude <75137537+NodudeWasTaken@users.noreply.github.com>
2024-09-14 03:38:06 +0200

    deboucne properfd

--- index.js
+++ index.js
@@ -50,23 +50,8 @@ eventSource.on(event_types.MESSAGE_RECEIVED, handleIncomingMessage);
 eventSource.on(event_types.MESSAGE_UPDATED, handleIncomingMessage);
 eventSource.on(event_types.SMOOTH_STREAM_TOKEN_RECEIVED, handleSmoothMessage);
 
-const debounce = (mainFunction, delay) => {
-	// Declare a variable called 'timer' to store the timer ID
-	let timer;
-
-	// Return an anonymous function that takes in any number of arguments
-	return function (...args) {
-		// Clear the previous timer to prevent the execution of 'mainFunction'
-		clearTimeout(timer);
-	
-		// Set a new timer that will execute 'mainFunction' after the specified delay
-		timer = setTimeout(() => {
-			mainFunction(...args);
-		}, delay);
-	};
-};
-
 let running = false
+let timer;
 const handy = new Handy.default();
 async function handleCommand(msg) {
 	try {
@@ -87,12 +72,17 @@ async function handleCommand(msg) {
 				running = true
 				await handy.setHampStart();
 			}
-			debounce(() => {
+
+			// Clear the previous timer to prevent the execution of 'mainFunction'
+			clearTimeout(timer);
+		
+			// Set a new timer that will execute 'mainFunction' after the specified delay
+			timer = setTimeout(() => {
 				console.log("handy stop")
 				handy.setHampStop().then(() => {
 					running = false
 				})
-			}, delay)()
+			}, delay);
 		}
 
 		return matched
